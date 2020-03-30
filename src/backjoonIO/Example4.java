184db83131db0ed8a66553607358e40a2b15a2d6
cogito1016 <cogito1016@naver.com>
2020-03-30 13:57:39 +0900

    Clear exmaple4

---
+++ src/backjoonIO/Example4.java
@@ -0,0 +1,19 @@
+package backjoonIO;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNext()) {
+			int a = scan.nextInt();
+			int b= scan.nextInt();
+			System.out.println(a+b);
+		}//while end;
+	}//main() end
+}//Example4 end
