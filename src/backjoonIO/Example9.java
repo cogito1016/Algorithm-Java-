66b9de7c10acddcea007b8950f7f9fdb618bc8ca
cogito1016 <cogito1016@naver.com>
2020-04-01 00:28:21 +0900

    Clear example9

---
+++ src/backjoonIO/Example9.java
@@ -0,0 +1,37 @@
+package backjoonIO;
+
+
+import java.util.Scanner;
+
+public class Example9 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		String inpStr ;
+		int max=100;
+		boolean condition=true;
+		
+		while(scan.hasNext()&&max>0) {
+			inpStr=scan.nextLine();
+			
+			
+			for(int i=0;i<inpStr.length();i++) {
+				char ch = inpStr.charAt(i);
+				if( (ch>64 && ch<91 ) || (ch>96 && ch<123 ) ||
+						(ch>47 && ch<58 )|| (ch==32)){
+				}else {
+					condition=false;
+					break;
+				}//if~else end
+			}
+			
+
+			if(condition==true) {
+				System.out.println(inpStr);
+			}else {
+				break;
+			}
+			max--;
+		}//while end
+	}//main() end
+}
