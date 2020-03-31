b59cec6b898b853792745d0562a48d18b6369c65
cogito1016 <cogito1016@naver.com>
2020-03-31 15:14:51 +0900

    Clear example12

---
+++ src/backjoonIO/Example12.java
@@ -0,0 +1,19 @@
+package backjoonIO;
+
+import java.util.Scanner;
+
+public class Example12 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan =
+				new Scanner(System.in);
+		
+		int inputNum = scan.nextInt();
+		for(int i=1; i<=inputNum; i++) {
+			System.out.println(i);
+		}//for end
+		
+
+	}//main() end
+}
