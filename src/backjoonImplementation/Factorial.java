7d66b473e9aea4e1747bb8b4f961495c33b9d155
cogito1016 <cogito1016@naver.com>
2020-04-13 16:54:42 +0900

    Clear Factorial

--- src/backjoonImplementation/Factorial.java
+++ src/backjoonImplementation/Factorial.java
@@ -1,13 +1,25 @@
 package backjoonImplementation;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Factorial {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int inputNum =  Integer.parseInt(br.readLine());
 		
+		long result = 1;
 		
+		for(int i=1;i<=inputNum;i++) {
+			result*=i;
+		}//for end
+		
+		System.out.println(result);
+		
+		br.close();
 	}//main() end
 }//class end
