006d6eada0e84637ee11a5154255300b80dc90ef
cogito1016 <cogito1016@naver.com>
2020-04-18 23:47:24 +0900

    Fail.. Need to learn BinarySearch clearly

--- src/backjoonBfsDfs/BfsLaserPoint.java
+++ src/backjoonBfsDfs/BfsLaserPoint.java
@@ -10,7 +10,7 @@ import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Bfs_DfsLaserPoint_OverMemoryFix {
+public class BfsLaserPoint {
 	static int[][] rootMap;
 	static class Node{
 		int x,y,element;
@@ -44,19 +44,18 @@ public class Bfs_DfsLaserPoint_OverMemoryFix {
 				if(map[i][j]=='C') {
 					startX=j;
 					startY=i; 
-					rootMap[i][j]=-1;
 				}//if end
 			}//for end
 		}//for end
 		
 		queue.offer(new Node(startY,startX));
 		visit[startY][startX]=true;
-		int minDistance=0;
-		Node node;
+		int mirrorNum=0;
+		Node node = null;
+		int beforeX=startX,beforeY=startY;
 		
 		while(!queue.isEmpty()) {
 			int queueSize = queue.size();
-			minDistance++;
 			for(int i=0;i<queueSize;i++) {
 				if(queue.isEmpty())
 					break;
@@ -77,78 +76,36 @@ public class Bfs_DfsLaserPoint_OverMemoryFix {
 							queue.poll();
 						break;
 					}//if end
-					rootMap[thisY][thisX]=minDistance;
+					
+
+					int subX = Math.abs(beforeX-thisX); 
+					int subY = Math.abs(beforeY-thisY); 
+					if(subX==1&&subY==1) {
+						int thisVal = rootMap[node.y][node.x];
+						int nextVal = rootMap[thisY][thisX];
+						if(nextVal!=0)
+						rootMap[thisY][thisX]= rootMap[node.y][node.x]+1;
+						}
+					else
+						rootMap[thisY][thisX]=rootMap[node.y][node.x];
+					
 					visit[thisY][thisX]=true;
 					queue.offer(new Node(thisY,thisX));
+					
 				}//for end
 			}//for end
+			beforeX=node.x;
+			beforeY=node.y;
 		}//while end
 
-		rootMap[startY][startX]=0;
-		visit = new boolean[y][x];
-		stack.add(new Node(startY,startX));
-		visit[startY][startX]=true;
-		
-		
-		List<Node> rootList = new ArrayList<Node>();
-		int result=0;
-		short[][] otherDirect = {{1,1},{1,-1},{-1,1},{-1,-1}};
-		int min=1000; 
-		int subY,subX,thisX,thisY,thisNum;;
-		Node beforeNode,nextNode;
-		
-		while(!stack.isEmpty()) {
-			node = stack.pop();
-			
-			for(Node nodeTemp : rootList) {
-				if(nodeTemp.element==node.element) {
-					int size = rootList.size()-rootList.indexOf(nodeTemp);
-					for(int i=0;i<size;i++) 
-						rootList.remove(rootList.size()-1);
-					break;
-				}//if end
-			}//for end
-			
-			rootList.add(node);
-			
-			for(int i=0;i<4;i++) {
-				thisX= node.x+direct[i][1];
-				thisY= node.y+direct[i][0];
-				
-				if(thisY>=y||thisY<0||thisX>=x||thisX<0) {
-					continue;}
-				thisNum=rootMap[thisY][thisX];
-				if(thisNum==0)
-					continue;
-				if(visit[thisY][thisX])
-					continue;
-				if(thisNum==-1) {
-					result=0;
-					rootList.add(new Node(thisY,thisX));
-					for(int a= 1; a<rootList.size()-1;a++) {
-						beforeNode = rootList.get(a-1);
-						nextNode = rootList.get(a+1);
-						subY = nextNode.y - beforeNode.y;
-						subX = nextNode.x - beforeNode.x;
-						for(int b=0;b<4;b++) {
-							if((subY==otherDirect[b][0] && subX==otherDirect[b][1]))
-									result++;
-						}//for end				
-					}	//for end
-					if(result<min)
-						min=result;
-					visit= new boolean[y][x];
-				}//if end
-				if(thisNum==minDistance)
-					continue;
-				if(thisNum!=node.element+1)
-					continue;
-				visit[thisY][thisX]=true;
-				stack.add(new Node(thisY,thisX));
-			}//for end
-		}//while end
+		for(int i=0;i<y;i++) {
+			for(int j=0;j<x;j++) {
+				System.out.print(rootMap[i][j]);
+			}
+			System.out.println();
+		}
 			 
-		System.out.println(min);
+		System.out.println(mirrorNum);
 		br.close();
 	}//main() end
 }//class end
---
+++ src/backjoonBinarySearch/FindNumber.java
@@ -0,0 +1,62 @@
+package backjoonBinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class FindNumber {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int numSize = Integer.parseInt(br.readLine());
+		int[] numList = new int[numSize];
+		st=new StringTokenizer(br.readLine());
+		for(int i=0;i<numList.length;i++) {
+			numList[i]=Integer.parseInt(st.nextToken());
+		}//for end
+		
+		Arrays.sort(numList);
+		
+//		int compareNumSize = Integer.parseInt(br.readLine());
+//		int[] compareNumList = new int[compareNumSize];
+//		st=new StringTokenizer(br.readLine());
+//		for(int i=0;i<compareNumList.length;i++) {
+//			compareNumList[i]=Integer.parseInt(st.nextToken());
+//		}//for end
+		
+		int left = 0;
+		int right = numSize-1;
+		int mid;
+		
+		
+		int findNum = 4 ;
+		
+		int leftPin=-1,rightPin=-1;
+		if(numList[left]==findNum)
+			leftPin=left;
+		if(numList[right]==findNum)
+			rightPin=right;
+		
+		int result=0;
+		while(leftPin!=-1&&rightPin!=-1) {
+			
+			mid = (left+right)/2;
+			
+			if(numList[mid]>findNum)
+				right = mid-1;
+			if(numList[mid]<findNum)
+				left = mid+1;
+			
+		}//while end
+		
+		
+		System.out.println(result);
+		
+		br.close();
+	}//main() end
+}//class end
