1d51f5222fb9c7d1a262d895b9808916a53b8dc4
cogito1016 <cogito1016@naver.com>
2020-04-24 09:05:39 +0900

    Clear TermProject using Dfs

---
+++ src/backjoonBinarySearch/NumberK.java
@@ -0,0 +1,19 @@
+package backjoonBinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberK {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int size = Integer.parseInt(br.readLine());
+		int k = Integer.parseInt(br.readLine());
+		
+		
+		
+		br.close();
+	}//main() end
+}//class end
