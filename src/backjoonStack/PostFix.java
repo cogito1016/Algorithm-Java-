716d20590e0c41a7bc2f98562bb461835fa24b02
cogito1016 <cogito1016@naver.com>
2020-05-05 17:54:58 +0900

    Fail Profix.

--- src/backjoonStack/PostFix.java
+++ src/backjoonStack/PostFix.java
@@ -12,37 +12,49 @@ public class PostFix {
 		
 		String statement = br.readLine();
 		Stack<Character> stack = new Stack<Character>();
+		boolean highFlag = false;
 		
-		boolean firstFlag=false;
 		for(int i=0;i<statement.length();i++) {
-			char c = statement.charAt(i);
-			if(c>=65 && c<=90) {
-				System.out.print(c);
-				if(firstFlag) 
+			char thisChar = statement.charAt(i);
+			if(thisChar>=65&&thisChar<=90) {
+				System.out.print(thisChar);
+				if(highFlag) {
 					System.out.print(stack.pop());
-				firstFlag=false;
-				
-			}else {
-				if(c==')')
-				{
-					while(stack.peek()!='(')
-						System.out.print(stack.pop());
-					stack.pop();
-				}else {			
-					if(stack.peek()=='+'||stack.peek()=='-')
-						System.out.print(stack.pop());
-					stack.push(c);
+					highFlag=false;
 				}
-
-				if(c=='*'||c=='/')
-					firstFlag=true;
-				else
-					firstFlag=false;
+				continue;
+			}//피연산자
+			
+			highFlag=false;
+			
+			
+			if(thisChar=='+'||thisChar=='-') {
+				if(!stack.isEmpty()) {
+					char beforeChar = stack.peek();
+					if(beforeChar=='+'||beforeChar=='-')
+						System.out.print(stack.pop());
+				}
+				stack.add(thisChar);
+				continue;
+			}//낮은 우선순위일때
+			 
+			if(thisChar=='*'||thisChar=='/'){
+				highFlag=true;
+				stack.add(thisChar);
+				continue;
+			}//높은 우선순위일때
+			
+			if(thisChar==')') {
+				char beforeChar = stack.peek();
+				if(beforeChar=='+'||beforeChar=='-')
+					System.out.print(stack.pop());
+				stack.pop();
+				continue;
 			}
+			
+			if(thisChar=='(')
+				stack.add(thisChar);
 		}//for end
-		while(!stack.isEmpty())
-			System.out.print(stack.pop());
-		
 		br.close();
 	}//main() end
 }//class end
