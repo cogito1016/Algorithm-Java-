18feffeb407d6778cb7b0d3b59ad33e7bdb37bee
cogito1016 <cogito1016@naver.com>
2020-05-05 19:00:24 +0900

    Fail Profix. Stop and resolve the problem later..

--- src/backjoonStack/PostFix.java
+++ src/backjoonStack/PostFix.java
@@ -3,6 +3,8 @@ package backjoonStack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class PostFix {
@@ -11,50 +13,66 @@ public class PostFix {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String statement = br.readLine();
-		Stack<Character> stack = new Stack<Character>();
-		boolean highFlag = false;
+		
+		Stack<Stack<Character>> stackList = new Stack<Stack<Character>>();
+		stackList.add(new Stack<Character>());
+		Stack<Character> thisStack;
+		
+		boolean lastFlag =false;
 		
 		for(int i=0;i<statement.length();i++) {
+			
+			thisStack = stackList.peek();
 			char thisChar = statement.charAt(i);
+
+			if(i==statement.length()-1) {
+				lastFlag=true;
+			}//if end
+			
 			if(thisChar>=65&&thisChar<=90) {
 				System.out.print(thisChar);
-				if(highFlag) {
-					System.out.print(stack.pop());
-					highFlag=false;
-				}
-				continue;
+				if(!thisStack.isEmpty()) {
+					char beforeChar = (char) thisStack.peek();
+					if(beforeChar=='*'||beforeChar=='/')
+						System.out.print(thisStack.pop());
+					else if((beforeChar=='+'||beforeChar=='-')&&lastFlag)
+						System.out.print(thisStack.pop());
+				}//if end
 			}//피연산자
-			
-			highFlag=false;
-			
-			
-			if(thisChar=='+'||thisChar=='-') {
-				if(!stack.isEmpty()) {
-					char beforeChar = stack.peek();
+			else if(thisChar=='+'||thisChar=='-') {
+				if(!thisStack.isEmpty()) {
+					char beforeChar = (char) thisStack.peek();
 					if(beforeChar=='+'||beforeChar=='-')
-						System.out.print(stack.pop());
-				}
-				stack.add(thisChar);
-				continue;
-			}//낮은 우선순위일때
-			 
-			if(thisChar=='*'||thisChar=='/'){
-				highFlag=true;
-				stack.add(thisChar);
-				continue;
-			}//높은 우선순위일때
-			
-			if(thisChar==')') {
-				char beforeChar = stack.peek();
-				if(beforeChar=='+'||beforeChar=='-')
-					System.out.print(stack.pop());
-				stack.pop();
-				continue;
-			}
-			
-			if(thisChar=='(')
-				stack.add(thisChar);
+						System.out.print(thisStack.pop());
+				}//if end
+				thisStack.add(thisChar);
+			}//'하'순위 연산자
+			else if(thisChar=='*'||thisChar=='/') {
+				thisStack.add(thisChar);
+			}//'중'순위 연산자
+			else if(thisChar=='(') {
+				stackList.add(new Stack<Character>());
+			}//'상'순위 연산자
+			else if(thisChar==')') {
+				if(!thisStack.isEmpty()) {
+					char beforeChar = (char) thisStack.peek();
+					if(beforeChar=='+'||beforeChar=='-')
+						System.out.print(thisStack.pop());
+				}//if end
+				stackList.pop();
+				
+				thisStack= stackList.peek();
+//				if(!thisStack.isEmpty()) {
+//					char beforeChar = (char) thisStack.peek();
+//					if(beforeChar=='+'||beforeChar=='-')
+//						System.out.print(thisStack.pop());
+//				}//if end
+				while(!thisStack.isEmpty())
+					System.out.print(thisStack.pop());
+			}//'상'순위 연산자
 		}//for end
+		
+		
 		br.close();
 	}//main() end
 }//class end
