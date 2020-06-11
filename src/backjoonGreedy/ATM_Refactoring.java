7962495e288609f74842a9d3cd359b0e231cd547
cogito1016 <cogito1016@naver.com>
2020-06-11 21:21:09 +0900

    ATM refactoring

---
+++ src/backjoonGreedy/ATM_Refactoring.java
@@ -0,0 +1,34 @@
+package backjoonGreedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ATM_Refactoring {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int[] timeList = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			timeList[i] = Integer.parseInt(st.nextToken());
+		}//for end
+		
+		Arrays.sort(timeList);
+		
+		int result = 0;
+		for(int i=0;i<N;i++) {
+			result += timeList[i] * (N-i);
+		}//for end 
+		
+		System.out.println(result);
+		
+		br.close();
+	}//main() end
+}//class end 
