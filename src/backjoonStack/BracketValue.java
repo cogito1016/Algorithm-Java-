8896f36032bef0c3bdb65d6603f630be8fa432ef
cogito1016 <cogito1016@naver.com>
2020-06-16 16:41:23 +0900

    Why is this runtime Error????? ??????? - -

---
+++ src/backjoonStack/BracketValue.java
@@ -0,0 +1,76 @@
+package backjoonStack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BracketValue {
+
+	//(40 )41 [91 ]93
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		Stack<Character> stack = new Stack<Character>();
+		
+		if(s.length()==0||s.length()>30) {
+			System.out.println(0);
+			return ;
+		}
+		
+		stack.push(s.charAt(0));
+		char beforeC = s.charAt(0);
+		int level = 1;
+		int[] levelArr = new int[50];
+				
+		for(int i=1;i<s.length();i++) {
+			char c = s.charAt(i);
+			
+			if(c=='[' || c=='(') {
+				level++;
+				stack.push(c);
+				beforeC=c;
+				continue;
+			}//if end 열때
+
+			if(stack.isEmpty()) {
+				stack.push(c);
+				beforeC=c;
+				continue;
+			}//if end 비어있을때
+			
+			char checkC = stack.peek();
+			if(c==')' && checkC=='(') {
+				stack.pop();
+				if(beforeC=='(')
+					levelArr[level]+=2;
+				else {
+					levelArr[level]+=levelArr[level+1]*2;
+					levelArr[level+1]=0;
+				}
+			}//if end 소괄호 닫을때
+			
+			if(c==']' && checkC=='[') {
+				stack.pop();
+				if(beforeC=='[')
+					levelArr[level]+=3;
+				else {
+					levelArr[level]+=levelArr[level+1]*3;
+					levelArr[level+1]=0;
+				}
+			}//if end 대괄호 닫을때
+			
+			level--;
+			beforeC=c;
+		}//for end
+		
+		
+		if(stack.isEmpty()) 
+			System.out.println(levelArr[1]);
+		else
+			System.out.println(0);
+		
+		br.close();
+	}//main() end
+}//class end
