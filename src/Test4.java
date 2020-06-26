54a8126a2846c04d333a92613c91f27fac604776
cogito1016 <cogito1016@naver.com>
2020-06-26 22:17:20 +0900

    SquaredNumber with dp .. too hard T_T OMG OMG OMG OMG OMG

--- src/Test4.java
+++ src/Test4.java
@@ -4,5 +4,8 @@ public class Test4 {
 	public static void main(String[] args) {
 		
 		System.out.println(11*11*11);
+		
+		System.out.println((long)Math.sqrt(99999));
+		System.out.println( (long)Math.pow((long)Math.sqrt(99999), 2));
 	}//main() end
 }
--- src/backjoonDynamicProgramming/SquaredNumber.java
+++ src/backjoonDynamicProgramming/SquaredNumber.java
@@ -11,15 +11,35 @@ public class SquaredNumber {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N+1];
-		dp[1]=1;
 
 		int rootResult =  (int) Math.sqrt(N);
-		int powResult = (int) Math.pow(rootResult, 2);
+		int[] dp = new int[rootResult+1];
+		int[] minList = new int[N+1];
 		
+		for(int i=1;i<=N;i++) { //현재 타겟 수
+			int min = i;
+			
+			for(int j=1;j<=dp.length;j++) { //현재 제곱 수
+				int pow = (int) Math.pow(j,2);
+				if(i<pow)
+					break;
+				
+				int top = i/pow;
+				int bottom = i%pow;
+				
+				if(bottom==0)
+					dp[j]=top;
+				else
+					dp[j]=minList[pow]*top+minList[bottom];
+				
+				if(dp[j]<min)
+					min=dp[j];
+			}//for end
+			minList[i]=min;
+		}//for end
 		
+		System.out.println(minList[N]);
 		
-		System.out.println(dp[N]);
 		br.close();		
 	}//main() end
 }//class end
