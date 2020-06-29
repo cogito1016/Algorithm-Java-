0fb3fb1e2a309c2098dfafc2b24e9c1dccd0c891
cogito1016 <cogito1016@naver.com>
2020-06-30 01:39:11 +0900

    1,2,3Sum with dp Refactoring -4ms

--- src/backjoonDynamicProgramming/OneTwoThreeSum.java
+++ src/backjoonDynamicProgramming/OneTwoThreeSum.java
@@ -8,20 +8,28 @@ public class OneTwoThreeSum {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
+		int[] result = new int[T];
+		int max = 0;
 		
-		while(T-- >0) {
+		for(int i=0;i<T;i++) {
 			int n = Integer.parseInt(br.readLine());
-			int[] dp = new int[n+3];
-			dp[1]=1;
-			dp[2]=2;
-			dp[3]=4;
-			
-			for(int i=4;i<=n;i++) {
-				dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
-			}//for end
-			
-			System.out.println(dp[n]);
-		}//while end
+			result[i] = n;
+			if(max<n)
+				max=n;
+		}//for end
+		
+		int[] dp = new int[max+3];
+		dp[1]=1;
+		dp[2]=2;
+		dp[3]=4;
+		for(int i=4;i<=max;i++) {
+			dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
+		}//for end
+		
+		for(int item : result) {
+			System.out.println(dp[item]);
+		}//for end
+		
 		br.close();
 	}//main() end
 }//class end
