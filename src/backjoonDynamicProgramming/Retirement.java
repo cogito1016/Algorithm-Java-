07d763ec8abc54b859d286f14088ee8c7e58f5d2
cogito1016 <cogito1016@naver.com>
2020-07-10 19:23:18 +0900

    bug: 퇴사 -조합으로 풀려고 시도, 그러나 설계오류인듯 싶음. 조합내부논리수정필요

--- src/backjoonDynamicProgramming/Retirement.java
+++ src/backjoonDynamicProgramming/Retirement.java
@@ -2,6 +2,8 @@ package backjoonDynamicProgramming;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Retirement {
@@ -15,13 +17,53 @@ public class Retirement {
 		int[] P = new int[N];
 		
 		StringTokenizer st;
+		Stack<Integer> stack = new Stack<Integer>();
+		
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			T[i]=Integer.parseInt(st.nextToken());
 			P[i]=Integer.parseInt(st.nextToken());
+			
 		}//for end
 		
+		int max =0;
+		stack.add(-1);
+		int neededDays = 1;
+		int attainableValue = 0;
+		int resultValue = 0;
+		while(!stack.isEmpty()) {
+			if(stack.size()==4 && stack.get(1)==5) {
+				System.out.println("hi fuck");
+			}
+			int thisNum = stack.pop();
+
+			
+			if(thisNum!=-1) {
+				neededDays = T[thisNum];
+				attainableValue = P[thisNum];
+			}//if end
+			
+			resultValue -= attainableValue;
+			
+			for(int i=thisNum+1;i<N;i++) {
+				neededDays--;
+				stack.add(i);
+				if(neededDays!=0)
+					continue;
+				if(i+T[i]>=N+1)
+					continue;
+				
+				neededDays=T[i];
+				resultValue+=P[i];
+				
+				if(max<resultValue)
+					max=resultValue;
+			}//for end
+			
+			System.out.println(stack);
+		}//while end
 		
+		System.out.println(max);
 		br.close();
 	}//main() end
 	
