cc46590d2dd5a3157a778d0125457b63b61f26a4
cogito1016 <cogito1016@naver.com>
2020-07-20 17:48:02 +0900

    구현: 음계 -문제해결을 위해 최초의 숫자가 무언지, 순서가 보장되어있는지를 판단함

---
+++ src/backjoonDataStructure/MusicalScale.java
@@ -0,0 +1,39 @@
+package backjoonDataStructure;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MusicalScale {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int thisNum = Integer.parseInt(st.nextToken());
+		
+		int result = thisNum ;
+		
+			
+		for(int i=0;i<7;i++) {
+			int nextNum = Integer.parseInt(st.nextToken());
+			if(Math.abs(thisNum-nextNum)!=1) {
+				result = 0;
+				break;
+			}//if end
+			thisNum = nextNum;
+		}//for end
+		
+		if(result == 8) {
+			System.out.println("descending");
+		}else if(result == 1) {
+			System.out.println("ascending");
+		}else
+			System.out.println("mixed");
+		
+		
+		br.close();
+		
+	}//main() end
+}
