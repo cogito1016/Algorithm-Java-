d57d9f7f5bbb6662772a58763f84a9a024ec5848
cogito1016 <cogito1016@naver.com>
2020-07-20 18:09:54 +0900

    구현: 블랙잭 -카드3개를 조합으로 구현 *시간이 느림 리팩토링가능한지 살펴보고 시간단축시킬것

---
+++ src/backjoonFullSearch/BlackJack.java
@@ -0,0 +1,56 @@
+package backjoonFullSearch;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BlackJack {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] cards = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			cards[i] = Integer.parseInt(st.nextToken()); 
+		}//for end
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		int count = 0;
+		int max = 0;
+		int tempResult = 0;
+		stack.add(-1);
+		
+		while(!stack.isEmpty()) {
+			int thisNum = stack.pop();
+			
+			if(thisNum!=-1) {
+				count --;
+				tempResult -= cards[thisNum];
+			}//if end
+			
+			for(int i=thisNum+1;i<N;i++) {
+				tempResult += cards[i];
+				count ++ ;
+				stack.add(i);
+				
+				if(count==3) {
+					if(tempResult>max && tempResult<=M)
+						max=tempResult;
+					break;
+				}//if end
+			}//for end
+			
+		}//while end
+		System.out.println(max);
+		br.close();
+	}//main() end
+}
--- src/backjoonFullSearch/MusicalScale.java
+++ src/backjoonFullSearch/MusicalScale.java
@@ -1,4 +1,4 @@
-package backjoonDataStructure;
+package backjoonFullSearch;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
