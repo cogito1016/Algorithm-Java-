f06579e2c8410791720ee29e6728d5effc3613ff
cogito1016 <cogito1016@naver.com>
2020-07-21 20:25:07 +0900

    test: Integer래퍼클래스의 128, 127 비교연산 -래퍼클래스의 ==비교는 자동으로 값비교라고 생각했던것이 나의 오류. 결과적으로 래퍼클래스도 객체처럼 레퍼런스비교이다. 하지만 래퍼클래스는 특이하게 자체적으로 캐싱해두는 값의 범위가 있어서 해당 범위내에 접근한다면 같은 인스턴스를 반환해준다. 그래서 값비교가 되는건줄 착각했나보다. 꼭 유념해서 다음부터는 이런 실수가없도록 해야할 것. 래퍼클래스도 여타 다른 객체처럼 값비교시 equlas를 사용하거나 Integer의 경우 intValue를 사용할 것.

---
+++ src/test/WrapperClassTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WrapperClassTest {
+
+	@Test
+	public void Integer_비교_127이하() {
+		Integer i1 = 127;
+		Integer i2 = 127;
+		assertTrue(i1==i2);
+	}
+	
+	@Test
+	public void Integer_비교_128이상_1() {
+		Integer i1 = 128;
+		Integer i2 = 128;
+		assertTrue(i1==i2);
+	}
+	
+	@Test
+	public void Integer_비교_128이상_2() {
+		Integer i1 = 128;
+		Integer i2 = 128;
+		assertTrue(i1.equals(i2));
+	}
+
+}
