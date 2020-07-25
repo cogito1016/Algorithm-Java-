b3bda25cd6399a75fca1a5d276e34b0b2df1b425
cogito1016 <cogito1016@naver.com>
2020-07-26 00:47:07 +0900

    구현: 공유기설치 -List를 사용하는건 삭제 -의미상 '간격'이 탐색대상이므로, Left-Mid-Right -> Min-Avg-Max로 변경

--- src/backjoonBinarySearch/ShareDevice_Repeat.java
+++ src/backjoonBinarySearch/ShareDevice_Repeat.java
@@ -2,9 +2,7 @@ package backjoonBinarySearch;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class ShareDevice_Repeat {
@@ -18,25 +16,44 @@ public class ShareDevice_Repeat {
 		int C = Integer.parseInt(st.nextToken());
 		
 		int[] arr = new int[N];
-		List<Integer> deviceLoc = new ArrayList<Integer>();
 		
 		for(int i=0;i<N;i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}//for end 
 		
 		Arrays.sort(arr);
-		deviceLoc.add(0);
-		C--;
 		
-		int left = 0;
-		int right = arr.length-1;
-		int mid;
-		
-		while(left<=right) {
-			mid = (left+right)/2;
-		
-
+		int min = arr[1]-arr[0];
+		int max = arr[N-1]-arr[0];
+		int avg;
+		int result = 0;
+		while(min<=max) {
+			avg = (min+max)/2;
+			
+			int base = arr[0];
+			int installedDevice = 1;
+			int minDistance = 1000000000;
+			
+			for(int i=1;i<N;i++) {
+				int gap = arr[i]-base;
+				if(gap>=avg) {
+					installedDevice++;
+					base=arr[i];
+					if(minDistance>gap)
+						minDistance=gap;
+				}//if end
+			}//for end
+			
+			if(installedDevice>=C) {
+				min=avg+1;
+				if(minDistance>result)
+					result=minDistance;
+			}else if(installedDevice<C) {
+				max=avg-1;
+			}//if end
 		}//while end
+		
+		System.out.println(result);
 		
 		br.close();
 	}//main() end
