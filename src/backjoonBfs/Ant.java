c019667caea8034f36bc16493aa154dc83d0dc2a
cogito1016 <cogito1016@naver.com>
2020-07-26 17:32:32 +0900

    메모리초과: 개미 -BFS삭제 후 연산으로 최대한처리, 10억정수배열이 원인이라 생각

--- src/backjoonBfs/Ant.java
+++ src/backjoonBfs/Ant.java
@@ -2,23 +2,10 @@ package backjoonBfs;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Ant {
 	
-	
-	static class AntMan {
-		int num;
-		int loc;
-		int direct;
-		AntMan(int num, int loc,int direct){
-			this.num=num;
-			this.loc=loc;
-			this.direct=direct;
-		}//cons end	
-	}//class end 
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,44 +14,45 @@ public class Ant {
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
 		
-		Queue<AntMan> queue = new LinkedList<AntMan>();
+		int[] stick = new int[L];
 		
+		int time = 0;
+		int targetIdx = 0;
+		int value,tempTime;
 		
 		for(int i=1;i<=N;i++) {
-			int antLoc = Integer.parseInt(br.readLine());
 			
-			if(antLoc>0) {
-				queue.offer(new AntMan(i,antLoc,+1));
-			}else {
-				queue.offer(new AntMan(i,antLoc*-1,-1));
-			}//if~else end
+			value = Integer.parseInt(br.readLine());
+			tempTime = 0;
+			
+			if(value>0)
+				tempTime = L-value;
+			else
+				tempTime = value*-1;
+			
+			
+			if(tempTime>time) {
+				time=tempTime;
+				targetIdx = Math.abs(value);
+			}
+			stick[Math.abs(value)]= value>0?i:i*-1;
 		}//for end
 		
-		int lastAntNum = 0;
-		int time = 0;
-		
-		while(!queue.isEmpty()) {
+		int lastAntNum=Math.abs(stick[targetIdx]);
+		if(stick[targetIdx]==+1) {
 			
-			int queueSize = queue.size();
-			time++;
+			for(int i=targetIdx+1;i<L;i++) {
+				if(stick[i]<0)
+					lastAntNum=stick[i]*-1;
+			}
+		}else {
+			for(int i=targetIdx-1;i>0;i--) {
+				if(stick[i]>0)
+					lastAntNum=stick[i];
+			}
+		}//if ~ else end
 			
-			for(int i=0;i<queueSize;i++) {
-				AntMan ant = queue.poll();
-				
-				int direct = ant.direct;
-				int newLoc = ant.loc+direct;
-				
-				if(newLoc!=0&&newLoc!=L) {
-					queue.offer(new AntMan(ant.num,newLoc,direct));
-				}else {
-					lastAntNum = ant.num;
-				}//if~else end 
-				
-			}//for end 
-		}//while end 
-		
-		System.out.println(lastAntNum+" "+time);
-		
+		System.out.println(lastAntNum +" "+time);
 		
 		br.close();
 	}//main() end
