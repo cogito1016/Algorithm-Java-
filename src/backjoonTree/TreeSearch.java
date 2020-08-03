53bdc8a9455d9f3c53508d6d74280445ec5b33b3
cogito1016 <cogito1016@naver.com>
2020-08-03 19:49:13 +0900

    구현: 트리순회 -전위 후위 중위를 재귀로 구현 -트리구조 이해&구현 -노드가 트리의역할을 하는것이믐로 새로운 노드는 리스트에 리리, 기존에 있던 노드는 서로 이어주는 작업을 통해 마지막으로 최상위 노드를 구하여 사용해주었다

--- src/backjoonTree/TreeSearch.java
+++ src/backjoonTree/TreeSearch.java
@@ -8,139 +8,104 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class TreeSearch {
-
-	static class Tree{
-		Node head;
-		List<Node> list = new ArrayList<Node>();
-		
-		public void setHead(Node head) {
-			this.head=head;
-		}//setHead() end
-		
-		public Node findNode(String value) {
-			Node result = null;
-			
-			Stack<Node> stack = new Stack<Node>();
-			stack.add(head);
-			
-			while(!stack.isEmpty()) {
-				
-				Node node = stack.pop();
-				if(node.value.equals(value)) {
-					result = node;
-					break;
-				}
-				if(node.left!=null) {
-					stack.add(node.left);
-				}
-				if(node.right!=null) {
-					stack.add(node.right);
-				}
-			}//while end 
-
-			if(result==null) {
-				for(Node node : list) {
-					if(node.value.equals(value)) {
-						result=node;
-						break;
-					}//if end
-				}//for end
-				if(result==null) {
-					result=new Node();
-					result.value=value;
-					list.add(result);	
-				}//if end
-			}//if end
-			return result;
-		}//findNode() end
-		
-		public void preSearch(Node node) {
-			System.out.print(node.value);
-			if(node.left!=null)
-				preSearch(node.left);
-			if(node.right!=null)
-				preSearch(node.right);
-		}//preSearch() end
-		
-		public void midSearch(Node node) {
-			if(node.left!=null)
-				midSearch(node.left);
-			System.out.print(node.value);
-			if(node.right!=null)
-				midSearch(node.right);
-		}//midSearch() end
-		
-		public void postSearch(Node node) {
-			if(node.left!=null)
-				postSearch(node.left);
-			if(node.right!=null)
-				postSearch(node.right);
-			System.out.print(node.value);
-		}//postSearch() end
-	}
 	
 	static class Node{
 		String value;
-		Node left,right,parent;
-		Node(){}
-		Node(String value,Node left,Node right,Node parent){
-			this.value=value;
-			this.left=left;
-			this.right=right;
-			this.parent=parent;
-		}
-	}
+		Node parent, leftChild, rightChild;
+		
+		void preSearch(Node node) {
+			System.out.print(node.value);
+			if(node.leftChild!=null)
+				preSearch(node.leftChild);
+			if(node.rightChild!=null)
+				preSearch(node.rightChild);
+		}//preSearch() end
+		
+		void midSearch(Node node) {
+			if(node.leftChild!=null)
+				midSearch(node.leftChild);
+			System.out.print(node.value);
+			if(node.rightChild!=null)
+				midSearch(node.rightChild);
+		}//midSearch() end
+		
+		void postSearch(Node node) {
+			if(node.leftChild!=null)
+				postSearch(node.leftChild);
+			if(node.rightChild!=null)
+				postSearch(node.rightChild);
+			System.out.print(node.value);
+		}//postSearch() end
+		
+	}//Node class end 
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		Tree tree =new Tree();
+		int T = Integer.parseInt(br.readLine());
 		
-		st = new StringTokenizer(br.readLine());
-		Node head = new Node();
-		head.value=st.nextToken();
+		List<Node> nodeList = new ArrayList<Node>();
 		
-		Node headLeft = new Node();
-		headLeft.value = st.nextToken();
-		headLeft.parent = head;
-		
-		Node headRight = new Node();
-		headRight.value = st.nextToken();
-		headRight.parent = head;
-		
-		head.left=headLeft;
-		head.right=headRight;
-		
-		tree.setHead(head);
-		
-		
-		
-		for(int i=1;i<N;i++) {
+		while(T-- >0) {
 			st = new StringTokenizer(br.readLine());
-			String value = st.nextToken();
-			Node newNode = tree.findNode(value);
 			
-			String leftS = st.nextToken();
-			Node left = leftS.equals(".")? null : tree.findNode(leftS);
-			String rightS = st.nextToken();
-			Node right = rightS.equals(".")? null : tree.findNode(rightS);
+			String firstValue = st.nextToken();
 			
-			newNode.left=left;
-			if(left!=null) {
-				left.parent=newNode;
-			}
-			newNode.right=right;
-			if(right!=null) {
-				right.parent=newNode;
-			}
-		}//for end
+			Node firstNode=null;
+			for(Node node : nodeList) {
+				if(node.value.equals(firstValue)) {
+					firstNode=node;
+					break;
+				}//if end
+			}//for end
+			
+			if(firstNode==null) {
+				firstNode = new Node();
+				firstNode.value=firstValue;
+				nodeList.add(firstNode);
+			}//if end
+			
+			for(int i=0;i<2;i++) {
+				String nextValue = st.nextToken();
+				
+				Node nextNode = null;
+
+				if(!nextValue.equals(".")){
+					for(Node node : nodeList) {
+						if(node.value.equals(nextValue)) {
+							nextNode=node;
+						}//if end
+					}//for end
+					if(nextNode==null) {
+						nextNode = new Node();
+						nextNode.value=nextValue;
+						nodeList.add(nextNode);
+					}//if end
+				}//if end
+				
+				if(i==0) {
+					firstNode.leftChild=nextNode;
+				}else if(i==1) {
+					firstNode.rightChild=nextNode;
+				}//if~elseIf end
+
+				if(nextNode!=null)
+					nextNode.parent=firstNode;
+			}//for end 
+			
+		}//while end
 		
-		tree.preSearch(head);
+		Node node = nodeList.get(0);
+		while(node.parent!=null) {
+			node = node.parent;
+		}//while end
+		
+		node.preSearch(node);
 		System.out.println();
-		tree.midSearch(head);
+		node.midSearch(node);
 		System.out.println();
-		tree.postSearch(head);
+		node.postSearch(node);
+		
 		br.close();
 	}//main() end
 }
