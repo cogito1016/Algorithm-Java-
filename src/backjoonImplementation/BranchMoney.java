f63034294fa8ceb0f8103567e35b53fc416aa2fa
cogito1016 <cogito1016@naver.com>
2020-08-06 23:58:43 +0900

    구현중: 손익분기 -수학적 사고력문제

---
+++ src/backjoonImplementation/BranchMoney.java
@@ -0,0 +1,25 @@
+package backjoonImplementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BranchMoney {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A =Integer.parseInt(st.nextToken());
+		int B =Integer.parseInt(st.nextToken());
+		int C =Integer.parseInt(st.nextToken());
+		
+		int result = A/(C-B)+1;
+		
+		System.out.println(result);
+		
+		br.close();
+		
+	}//main() end
+}
