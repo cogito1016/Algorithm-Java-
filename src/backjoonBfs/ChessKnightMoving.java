864910c9b752957ecf01f91869cd1d47dc244125
cogito1016 <cogito1016@naver.com>
2021-05-04 19:49:51 +0900

    feat: ChessKnight Moving -시간 1/4 감소 -메모리 5000kb 감소 -결과를 담기위해 resultList를 생성해준 점이 메모리소모가 컸었던듯 -그런데 Queue processing level을 측정하기위한 로직을 이전방법에 비해  너무어렵게 짠거같다. 따라서 수정해보고 다시 제출해볼예정

---
+++ src/backjoonBfs/ChessKnightMoving.java
@@ -0,0 +1,89 @@
+package backjoonBfs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node{
+    int x;
+    int y;
+    Node(int y, int x){
+        this.x=x;
+        this.y=y;
+    }
+}
+
+public class ChessKnightMoving {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCaseNum = Integer.parseInt(br.readLine());
+
+        int[][] direction = {{-1,-2}, {-2,-1}, {-1,2}, {-2,1}, {1,-2}, {2,-1}, {2,1}, {1,2}};
+
+        while(testCaseNum-->0){
+            Queue<Node> queue = new LinkedList<>();
+            int N = Integer.parseInt(br.readLine());
+            boolean[][] visited = new boolean[N][N];
+
+            String[] kniteLoc = br.readLine().split(" ");
+            int startY = Integer.parseInt(kniteLoc[0]);
+            int startX = Integer.parseInt(kniteLoc[1]);
+
+            String[] goalLoc = br.readLine().split(" ");
+            int goalY = Integer.parseInt(goalLoc[0]);
+            int goalX = Integer.parseInt(goalLoc[1]);
+
+            queue.offer(new Node(startY,startX));
+            visited[startY][startX] = true;
+
+            int moveCnt = 0;
+            int beforeLevelCnt = 0;
+            int afterLevelCnt = 0;
+
+            while(!queue.isEmpty()){
+                Node presentNode = queue.poll();
+
+                if(presentNode.y==goalY && presentNode.x==goalX){
+                    break;
+                }
+
+                if(beforeLevelCnt>0){
+                    beforeLevelCnt--;
+                }
+
+                for(int i=0; i<8; i++){
+                    int y = presentNode.y + direction[i][0];
+                    int x = presentNode.x + direction[i][1];
+
+                    if(x>=N || y>=N || x<0 || y<0){
+                        continue;
+                    }
+                    if(visited[y][x]){
+                        continue;
+                    }
+
+                    afterLevelCnt++;
+                    visited[y][x] = true;
+                    queue.offer(new Node(y,x));
+                }
+
+                if(beforeLevelCnt==0){
+                    moveCnt++;
+                    beforeLevelCnt=afterLevelCnt;
+                    afterLevelCnt=0;
+                }
+            }
+
+            bw.write(moveCnt+"\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
