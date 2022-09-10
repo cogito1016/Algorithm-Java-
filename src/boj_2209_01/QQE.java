93e1364936f0171be47a4b17e48f033415c796b1
cogito1016 <cogito1016@naver.com>
2022-09-11 00:56:04 +0900

    feat: ??!

---
+++ src/boj_2209_01/QQE.java
@@ -0,0 +1,21 @@
+package boj_2209_01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class QQE {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine()+"??!";
+
+        bw.write(s);
+
+        br.close();
+        bw.close();
+    }
+}
