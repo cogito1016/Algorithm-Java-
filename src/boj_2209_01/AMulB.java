ef4bdd62707c696d38a581baf520f9fcb6bf9d32
cogito1016 <cogito1016@naver.com>
2022-09-10 23:33:19 +0900

    feat : A*B

---
+++ src/boj_2209_01/AMulB.java
@@ -0,0 +1,19 @@
+package boj_2209_01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class AMulB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+
+        bw.write((Integer.parseInt(input[0])*Integer.parseInt(input[1]))+"");
+        bw.close();
+    }
+}
