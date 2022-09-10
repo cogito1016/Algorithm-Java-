ab38821ff6d454e8d945caa98872a6cf7ba73037
cogito1016 <cogito1016@naver.com>
2022-09-10 19:20:20 +0900

    feat: Adapting...`

---
+++ src/boj_2209_01/WeLoveKiriii.java
@@ -0,0 +1,15 @@
+package boj_2209_01;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class WeLoveKiriii {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("강한친구 대한육군");
+        bw.newLine();
+        bw.write("강한친구 대한육군");
+        bw.close();
+    }
+}
